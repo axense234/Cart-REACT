6e26a92426b1b0497519c8cecf531f756dec53bb
axense234 <andreicomanescuonline@gmail.com>
2022-07-09 13:58:14 +0300

    Changed == to ===

--- src/App.js
+++ src/App.js
@@ -27,20 +27,20 @@ export const App = () => {
 
 const Navbar = ({ cartSize }) => {
   return (
-    <div className='navbar'>
-      <h1 id='company-name'>Axense's Team</h1>
+    <div className="navbar">
+      <h1 id="company-name">Axense's Team</h1>
       {/* Shopping Icon */}
       <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='16'
-        height='16'
-        fill='currentColor'
-        className='bi bi-cart'
-        viewBox='0 0 16 16'
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-cart"
+        viewBox="0 0 16 16"
       >
-        <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z' />
+        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
       </svg>
-      <div id='cart-size'>{cartSize}</div>
+      <div id="cart-size">{cartSize}</div>
     </div>
   );
 };
@@ -54,9 +54,9 @@ const MainContent = ({ state }) => {
   };
   return (
     <>
-      <main className='main-content'>
-        <h1 id='content-title'>YOUR BAG</h1>
-        <div className='items-content'>
+      <main className="main-content">
+        <h1 id="content-title">YOUR BAG</h1>
+        <div className="items-content">
           {state.cartItems.map((item) => {
             const { id, img, name, price } = item;
             return (
@@ -71,17 +71,17 @@ const MainContent = ({ state }) => {
           })}
         </div>
         {state.cartSize === 0 ? (
-          <p id='empty'>is currently empty</p>
+          <p id="empty">is currently empty</p>
         ) : (
-          <div className='total'>
-            <div className='total-info'>
-              <p id='total'>Total</p>
-              <p id='cost'>
+          <div className="total">
+            <div className="total-info">
+              <p id="total">Total</p>
+              <p id="cost">
                 <span>$</span>
                 {state.totalCost}
               </p>
             </div>
-            <button id='clear-cart' onClick={() => handleClearCart()}>
+            <button id="clear-cart" onClick={() => handleClearCart()}>
               CLEAR CART
             </button>
           </div>
@@ -115,59 +115,59 @@ const Item = ({ src, itemName, itemPrice, id }) => {
 
   return (
     <>
-      <div className='item-container'>
-        <img src={src} alt='' />
-        <div className='item-info'>
+      <div className="item-container">
+        <img src={src} alt="" />
+        <div className="item-info">
           <h2>{itemName}</h2>
           <p>
             <span>$ </span>
             {itemPrice}
           </p>
-          <button id='remove-item' onClick={() => handleRemove()}>
+          <button id="remove-item" onClick={() => handleRemove()}>
             Remove
           </button>
         </div>
-        <div id='size-button'>
+        <div id="size-button">
           <button
-            className='btn-part'
+            className="btn-part"
             onClick={() => {
               handleItemQuantity("up");
             }}
           >
             {/* Chevron Up */}
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              className='bi bi-chevron-up'
-              viewBox='0 0 16 16'
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-chevron-up"
+              viewBox="0 0 16 16"
             >
               <path
-                fillRule='evenodd'
-                d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'
+                fillRule="evenodd"
+                d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
               />
             </svg>
           </button>
-          {itemQuantity == 0 ? null : <p id='item-quantity'>{itemQuantity}</p>}
+          {itemQuantity === 0 ? null : <p id="item-quantity">{itemQuantity}</p>}
           <button
-            className='btn-part'
+            className="btn-part"
             onClick={() => {
               handleItemQuantity("down");
             }}
           >
             {/* Chevron Down */}
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              className='bi bi-chevron-down'
-              viewBox='0 0 16 16'
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-chevron-down"
+              viewBox="0 0 16 16"
             >
               <path
-                fillRule='evenodd'
-                d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
+                fillRule="evenodd"
+                d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
               />
             </svg>
           </button>
